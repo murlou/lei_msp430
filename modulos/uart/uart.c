2896f61246e1f1fa478c333ec1f1363cadad40da
pedrohgi <pedro.inazawa@gmail.com>
2013-02-03 17:24:08 -0200

    Adaptação do Uart.c
    adaptação do uart inicialize para buscar a macro da MCU desejada
    Adaptação para usar os pinos TX e Rx escolhidos no Uart.h (novo método)
    
    
    
    
    
    
    
    
    Adaptação do uart_set_bitrate: Quando escolhido o MCU, é criada uma Flag que irá definir 
    qual dos dois acionamentos irá ser feito. 
    A diferença básica entre ambos é a velocidade do clock interno de cada MSP430
    
    
    
    
    
    
    
    
    Adaptação das Funçoes de interrupção
    As macros irão definir qual função de interrupção utilizar agora. 

--- modulos/uart/uart.c
+++ modulos/uart/uart.c
@@ -4,16 +4,39 @@
 #error "Error! This MCU doesn't have a USCI peripheral"
 #endif /*__MSP430_HAS_USCI__*/
 
+/**
+ * uart_initialize() - Inicializa a UART.
+ *
+ *
+ */
 int uart_initialize()
 {
-
+	/**
+	*!TODO - Achar as portas corretas do G2553
+	*/
 #ifdef __MSP430G2553
-	P1SEL = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
+	/*P1SEL = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
 	P1SEL2 = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
-#endif /*__MSP430G2553*/
-
+	*/
+	P1SEL = TX_PIN | RX_PIN;
+	P1SEL2 = TX_PIN | RX_PIN; //!TODO - Não esquecer de definir os txpins e os rxpins
 	UCA0CTL1 |= UCSSEL1 | UCSWRST;        // Select SMCLK as clock source for UART
-					    // Disable USCI module for initializations
+	static unsigned int G2553_FLAG=1; // Flag que indicara ao configurador de baudrate qual será o dispositivo.
+
+//FEITO
+#elif __MSP430F2274
+	P3SEL = TX_PIN | RX_PIN  ;                     // P3.4 = tXD, P3.5=rXD
+	
+	UCA0CTL1 |= UCSSEL_2;                     // SMCLOCK
+	
+	
+	static unsigned int F2274_FLAG=1; // Flag que indicara ao configurador de baudrate qual será o dispositivo.
+//#endif /*__MSP430F2274*/
+
+#else
+	#error " Error! Não sei qual MCU está sendo utilizada, ou MCU não contemplada!"
+	
+#endif
 
 	UCA0BR0 = 104;                        // 9600 Baud with 1MHz clock (Refer- 
 	UCA0BR1 = 0;                          // 9600 Baud with 1MHz clock -Pg.15-22)
@@ -23,35 +46,68 @@ int uart_initialize()
 	IE2 |= UCA0RXIE + UCA0TXIE;                      // Enable USCI_A0 RX interrupt
 
 	return SUCCESS;
-
-
 }
 
+/**
+ * uart_set_bitrate() - Seta a velocidade da comunicação serial. 
+ *
+ */
 int uart_set_bitrate(unsigned int rate)
 {
-	BCSCTL1 = CALBC1_16MHZ;                    // Set DCO
-	DCOCTL = CALDCO_16MHZ;
+	if(F2274_FLAG==1)
+	
+		{
+			BCSCTL1 = CALBC1_1MHZ;                    // Set DCO
+			DCOCTL = CALDCO_1MHZ;
+			
+			UCA0BR0 = (long int)(1000000/rate); 	                         // 1MHz 9600
+			UCA0BR1 = ((long int)(1000000/rate) ) >> 8;                        // 1MHz 9600
+		
+			return (UCA0BR0 - (1000000*rate) ) + (UCA0BR1- ((1000000*rate) << 8) );
+		}
+	
+	
+	else if(G2553_FLAG=1)
+		{
+			BCSCTL1 = CALBC1_16MHZ;                    // Set DCO
+			DCOCTL = CALDCO_16MHZ;
 
-	UCA0BR0 = (long int)(16000000/rate); //104;                         // 16MHz 9600
-	UCA0BR1 = ((long int)(16000000/rate) ) >> 8;                        // 16MHz 9600
+			UCA0BR0 = (long int)(16000000/rate); //104;                         // 16MHz 9600
+			UCA0BR1 = ((long int)(16000000/rate) ) >> 8;                        // 16MHz 9600
 
-	return (UCA0BR0 - (16000000*rate) ) + (UCA0BR1- ((16000000*rate) << 8) );
+			return (UCA0BR0 - (16000000*rate) ) + (UCA0BR1- ((16000000*rate) << 8) );
+		};
+	
 }
 
+/**
+ * uart_close() - Desliga a comunicação serial. 
+ *
+ */
 int uart_close()
 {
+//Possivelmente funciona com ambos os MSP430
 	IE2 &= ~(UCA0RXIE + UCA0TXIE);                      // Desable USCI_A0 RX interrupt
 	
 	return IE2 && (UCA0RXIE + UCA0TXIE);
 }
 
+/**
+ * getchar()
+ *
+ */
 int getchar()
 {
+	//Possivelmente funciona com ambos os MSP430
 	while (!(IFG2&UCA0RXIFG));			// Wait recive a data
 
 	return UCA0RXBUF=='\r'?'\n' : UCA0RXBUF;
 }
 
+/**
+ * putchar() 
+ *
+ */
 int putchar(volatile int c)
 {
 	if(UCA0CTL1 & UCSWRST) return EOF; // USCI not active
@@ -70,7 +126,19 @@ int putchar(volatile int c)
  * Interrupr function from TX of USCI 0
  * It's just commute P1.0 ( OUTPUT LED )
  */
-interrupt(USCIAB0TX_VECTOR) USCI0TX_ISR(void)
-{
-	__bic_SR_register_on_exit(LPM0_bits + GIE);
-}
+#ifdef __MSP430G2553
+	interrupt(USCIAB0TX_VECTOR) USCI0TX_ISR(void)
+	{
+		__bic_SR_register_on_exit(LPM0_bits + GIE);
+	}
+#elif __MSP430F2274
+	#pragma vector=USCIAB0RX_VECTOR
+	__interrupt void USCI0RX_ISR(void)
+	{
+	  while (!(IFG2 & UCA0TXIFG));              // USCI_A0 TX buffer ready?
+	  UCA0TXBUF = UCA0RXBUF;                    // TX -> RX
+	}
+#endif
+
+
+
